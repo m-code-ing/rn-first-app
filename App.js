0a2623661ad5f9cfda1912863145d816c9b9de4b
Mayank Ashok <mayank@divertica.com>
2022-01-26 20:39:02 -0800

    inital commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  ScrollView,
+} from "react-native";
+import { borderColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = React.useState("");
+  const [courseGoals, setCourseGoals] = React.useState([]);
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <ScrollView>
+        {courseGoals.map((goal, index) => (
+          <Text key={index} style={styles.listItem}>
+            {goal}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderWidth: 1,
+    borderColor: "black",
   },
 });
